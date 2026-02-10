cf3e05f56c7549f55e8e655dd76ac8553ae0a81e
8myzxrhzf4-jpg <bique287@gmail.com>
2026-02-09 21:46:57 -0500

    Add main Spring Boot application class

---
+++ src/main/kotlin/com/casino/uniforms/UniformManager2Application.kt
@@ -0,0 +1,11 @@
+package com.casino.uniforms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UniformManager2Application
+
+fun main(args: Array<String>) {
+    runApplication<UniformManager2Application>(*args)
+}
